826c1667b2809f8dfcb27b7985bee2dcb694bf0f
magddevf <magddevf@gmail.com>
2024-12-05 14:49:05 +0200

    Initial Commit

---
+++ olofoofo-ios/olofoofo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  olofoofo_iosApp.swift
+//  olofoofo-ios
+//
+//  Created by Macbook on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct olofoofo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnBoardingView()
+        }
+    }
+}
